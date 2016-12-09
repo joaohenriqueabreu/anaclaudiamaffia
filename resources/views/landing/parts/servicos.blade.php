ce6d3bb641f03fe327171db18c2dadc1d189c78e
anacmaffia <arq.anaclaudiamaffia@gmail.com>
2016-12-09 11:21:18 -0200

    atualização dos textos

--- resources/views/landing/parts/servicos.blade.php
+++ resources/views/landing/parts/servicos.blade.php
@@ -188,12 +188,9 @@
                             <div class="g-heading-v9 g-mb-30">
                                 <h3>Consultoria</h3>
                             </div>
-                            <p class="g-mb-30" align="justify">
-                                Para os clientes que possuem dúvidas sobre como reorganizar ou decorar o seu espaço mas não querem ou não precisam contratar um projeto completo.
-                                A consultoria é uma visita técnica em que a profissional tira todas as dúvidas do cliente, apresenta solução para o layout, discute possibilidade de compra de móveis ou aproveitamento dos existentes,
-                                sugere acabamentos, pintura, decoração, indica fornecedores e entrega posteriormente por email um arquivo contendo tudo que foi discutido na visita e planta layout considerando as modificações.
-                                Para saber mais informações sobre esse serviço, entre em contato aqui pelo site!
-                            </p>
+                            <p class="g-mb-30" align="justify">Quer mudar o seu espaço contando com o auxílio de um profissional da área e ainda de maneira rápida, objetiva e com custos mais acessíveis? Precisa de ajuda para escolher um novo apartamento e ter certeza que ele atenderá a suas expectativas? Então esse serviço é ideal para você!</p>
+<p class="g-mb-30" align="justify">A consultoria de interiores é uma visita técnica em que vamos tirar todas as suas dúvidas, te orientar sobre o melhor layout para seu espaço, discutir possibilidades de compra de móveis ou aproveitamento dos existentes, sugerir acabamentos, pintura, itens decorativos, indicar fornecedores e tudo mais o que você achar necessário para executar todas as idéias e deixar o seu espaço como sempre planejou! Após a visita, ainda vamos te enviar um material orientativo sobre tudo que foi discutido na visita, assim você terá todas as informações para dar andamento à execução.</p>
+<p class="g-mb-30" align="justify">Além disso, também podemos ajudar aos clientes interessados em adquirir um imóvel! Se esse for o seu caso, podemos acompanhá-lo nas visitas aos imóveis, avaliar as condições dos itens existentes, confirmar se o espaço suportará os seus móveis existentes, identificar possibilidades de mudança no espaço para que fique conforme seus desejos e necessidades. Um investimento desse porte precisa ser cauteloso, e o olhar de um profissional treinado poderá evitar problemas e frustações futuros.</p>
                         </div>
                     </div>
                 </div>
@@ -205,16 +202,12 @@
                         </div>
                         <div class="col-sm-6 col-sm-push-1">
                             <div class="g-heading-v9 g-mb-30">
-                                <h3>Projeto de Arquitetura e Interiores</h3>
+                                <h3>Projeto arquitetônico, interiores e acompanhamento</h3>
                             </div>
-                            <p class="g-mb-30" align="justify">
-                                Projeto desenvolvido para novas edificações e/ou adequações de edificações existentes, o projeto arquitetônico leva em consideração a posição solar, ventilação, setorização,
-                                fluxo interno, legislação, limitações técnicas, hábitos e desejos dos futuros usuários. O projeto completo inclui etapas de implantação da construção ao terreno,
-                                desenvolvimento de anteprojeto, projeto legal, projeto executivo e compatibilização com os projetos complementares (hidráulico, elétrico, estrutural, entre outros projetos).
-                                </br>Já o projeto de interiores é desenvolvido para ambientes internos, sejam eles residenciais, comerciais ou corporativos.
-                                O intuito do projeto de interiores é organizar e planejar ambientes, dando personalidade ao espaço, de acordo com as intenções dos clientes e suas necessidades. Para atingir o resultado desejado é preciso conciliar padrões de estética, técnica, funcionalidade e custo. Inclui projetos de móveis feitos sob medida, projeto de iluminação, sugestão de novos revestimentos, pintura, mobiliário, cortinas, etc.
-                                Para saber mais informações sobre esse serviço, entre em contato aqui pelo site!
-                            </p>
+                            <p class="g-mb-30" align="justify">Você tem um sonho de planejar o seu lar e deixa-lo adequado às suas necessidades? Gostaria de fazer uma mudança ou reforma, mas não imagina por onde começar? Você quer mudar, mas não tem tempo para cuidar de todos os processos que uma obra gera? Então você precisa do auxílio de um arquiteto! E podemos te ajudar com tudo isso!</p>
+<p class="g-mb-30" align="justify">O pacote de projeto arquitetônico, projeto de interiores e acompanhamento é o serviço mais completo que oferecemos. Ele foi desenvolvido para te atender em todas as etapas da obra, desde o planejamento e concepção do projeto inicial até o acompanhamento e fiscalização da execução. </p>
+<p class="g-mb-30" align="justify">Seremos parceiros em todo o processo, nosso contato será constante desde o momento da concepção do projeto até a entrega final da obra. Você receberá imagens, desenhos, memorial descritivo, orçamentos, cronograma, relatórios, material completo e super detalhado de cada etapa. Você terá total segurança de que o projeto e a execução estarão de acordo com suas expectativas estéticas e de custo e prazo. Poderá solicitar revisões até que entenda que a proposta atende completamente aos seus sonhos!</p>
+<p class="g-mb-30" align="justify">O serviço de desenvolvimento de projeto arquitetônico, interiores e acompanhamento da execução poderá ser contratado por etapas, conforme suas necessidades e interesse. Mas cá entre nós, você terá suas preocupações reduzidas se permitir nosso acompanhamento total, sua obrigação será apenas de aproveitar e sonhar com cada detalhe proposto, afinal, o trabalho ficará por nossa conta!</p>
                         </div>
                     </div>
                 </div>
@@ -228,14 +221,9 @@
                             <div class="g-heading-v9 g-mb-30">
                                 <h3>Projeto Virtual</h3>
                             </div>
-                            <p class="g-mb-30" align="justify">
-                                O projeto virtual é o desenvolvimento a distância do projeto de interiores. Todo o contato é realizado através da internet.
-                                Primeiro a profissional orienta o cliente sobre como fazer e ele envia fotografias do espaço a ser modificado,
-                                tira as medidas do ambiente e móveis existentes e preenche um checklist orientativo sobre o que ele deseja para o seu projeto.
-                                Após uma reunião online a profissional desenvolve uma proposta, o cliente pode solicitar modificações e depois inicia-se a fase de detalhamento.
-                                O projeto é entregue completo, por email, com todas as medidas, informações de acabamentos e tudo mais o que for necessário para que o cliente possa executar sozinho.
-                                Para saber mais informações sobre esse serviço, entre em contato aqui pelo site!
-                            </p>
+                            <p class="g-mb-30" align="justify">Você mora fora da grande Belo Horizonte, mas se identificou com nossa maneira de trabalhar e gostaria de contar com o nosso auxílio para desenvolver o projeto ideal para você? Hoje felizmente podemos contar com a tecnologia a nosso favor e tornar esse desejo possível!</p>
+<p class="g-mb-30" align="justify">Todo o nosso contato será através da internet ou telefone, primeiro você nos enviará informações sobre o ambiente que será modificado, teremos reuniões online em que você vai nos dizer tudo que sempre sonhou para o seu espaço, quais os seus desejos e necessidades e a partir daí desenvolveremos seu projeto. Vamos seguir todos os passos do projeto tradicional, porém, será tudo online! E pode ficar bem tranquilo, você poderá solicitar ajustes e revisões, o que queremos é que você fique totalmente satisfeito! </p>
+<p class="g-mb-30" align="justify">Após aprovação do projeto você também receberá o material completo para a execução e estaremos sempre em contato para auxiliá-lo com as dúvidas que surgirem, tudo isso de maneira rápida e descomplicada!</p>
                         </div>
                     </div>
                 </div>
