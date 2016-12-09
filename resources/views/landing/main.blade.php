82940ff1a9db4cb8775e495d61f688853a4702ad
anacmaffia <arq.anaclaudiamaffia@gmail.com>
2016-12-09 11:04:51 -0200

    atualização texto das barras

--- resources/views/landing/main.blade.php
+++ resources/views/landing/main.blade.php
@@ -11,17 +11,17 @@
 @section('conteudo')
     @include('landing.parts.home')
     {{--// Temporario para depois alterar o texto--}}
-    @php ($mensagem = "Aqui vem a mensagem 111")
+    @php ($mensagem = "Realizar os SONHOS dos nossos clientes, superando sempre suas EXPECTATIVAS. Através de um atendimento DIFERENCIADO, mostrar a eles como o arquiteto pode ser um ALIADO, auxiliando em TODAS as etapas de desenvolvimento, levando QUALIDADE DE VIDA e CONFORTO ao seu dia a dia. Desenvolver um trabalho com PROFISSIONALISMO e RESPEITO ao cliente.")
     @include('landing.parts.mensagem')
 
     @include('landing.parts.perfil')
 
-    @php ($mensagem = "Aqui vem a mensagem 2")
+    @php ($mensagem = "Ser um escritório referência em QUALIDADE, INOVAÇÃO e DIFERENCIAÇÃO dos projetos. Colocar os SONHOS dos clientes sempre à frente, respeitando suas INDIVIDUALIDADES. Ser reconhecido por nossos clientes como PARCEIROS e agentes facilitadores para a realização de seus OBJETIVOS.")
     @include('landing.parts.mensagem')
 
     @include('landing.parts.servicos')
 {{--    @include('landing.parts.metodologia')--}}
-    @php ($mensagem = "SER ARQUITETO É FAZER DE SIMPLES TRAÇO A PROJEÇÃO DE SONHOS, A CONSTRUÇÃO DA REALIDADE, DO DESEJO EM ARTE CONCRETA")
+    @php ($mensagem = "TRANSPARÊNCIA, DISCIPLINA e COMPROMETIMENTO no atendimento aos clientes. ATENÇÃO e CUIDADO ao desenvolver cada projeto. Busca constante de CONHECIMENTO e INOVAÇÃO.")
     @include('landing.parts.mensagem')
 
     @include('landing.parts.diferencial')
