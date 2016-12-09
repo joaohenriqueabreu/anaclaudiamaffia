0db408b39d50d7834fc671cf0011de6e24970b96
anacmaffia <arq.anaclaudiamaffia@gmail.com>
2016-12-09 11:42:31 -0200

    Parametro background

--- resources/views/landing/main.blade.php
+++ resources/views/landing/main.blade.php
@@ -12,15 +12,18 @@
     @include('landing.parts.home')
     {{--// Temporario para depois alterar o texto--}}
     @php ($mensagem = "Realizar os SONHOS dos nossos clientes, superando sempre suas EXPECTATIVAS. Através de um atendimento DIFERENCIADO, mostrar a eles como o arquiteto pode ser um ALIADO, auxiliando em TODAS as etapas de desenvolvimento, levando QUALIDADE DE VIDA e CONFORTO ao seu dia a dia. Desenvolver um trabalho com PROFISSIONALISMO e RESPEITO ao cliente.")
+    @php ($background = "img/faq/barra1.jpg")
     @include('landing.parts.mensagem')
 
     @include('landing.parts.perfil')
 
     @php ($mensagem = "Ser um escritório referência em QUALIDADE, INOVAÇÃO e DIFERENCIAÇÃO dos projetos. Colocar os SONHOS dos clientes sempre à frente, respeitando suas INDIVIDUALIDADES. Ser reconhecido por nossos clientes como PARCEIROS e agentes facilitadores para a realização de seus OBJETIVOS.")
+    @php ($background = "img/faq/barra2.jpg")
     @include('landing.parts.mensagem')
 
     @include('landing.parts.diferencial')
     @php ($mensagem = "TRANSPARÊNCIA, DISCIPLINA e COMPROMETIMENTO no atendimento aos clientes. ATENÇÃO e CUIDADO ao desenvolver cada projeto. Busca constante de CONHECIMENTO e INOVAÇÃO.")
+    @php ($background = "img/faq/barra3.jpg")
     @include('landing.parts.mensagem')
     @include('landing.parts.servicos')
 {{--    @include('landing.parts.metodologia')--}}
