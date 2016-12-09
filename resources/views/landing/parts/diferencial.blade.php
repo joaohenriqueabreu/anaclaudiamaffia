ccf2d61abb5f037d9c66a28088ccca0b587c3415
anacmaffia <arq.anaclaudiamaffia@gmail.com>
2016-12-09 10:38:38 -0200

    Atualização dos textos

--- resources/views/landing/parts/diferencial.blade.php
+++ resources/views/landing/parts/diferencial.blade.php
@@ -5,8 +5,9 @@
         <div class="why-we-item why-we-item--light equal-height-column">
             <div class="why-we-item-inner">
                 <h2 class="h2"><em class="block-name">Diferenciais</em> Porque você deve fazer um projeto conosco ?</h2>
-                O projeto bem elaborado é resultado da parceria entre profissional e clientes.
-                É preciso entender bem as necessidades, sonhos e idéias do cliente para em conjunto chegar à uma solução que alie técnica, funcionalidade e estética.
+                Porque tratamos nossos clientes com o respeito e atenção que eles merecem, afinal nosso maior sonho é realizar os sonhos de cada um deles!
+                Porque buscamos criar espaços diferenciados, que realmente mudem a vida dos usuários, que tragam realização, conforto e praticidade.
+                Porque entendemos nossa tarefa enquanto arquitetos e responsáveis por trazer facilidades ao dia a dia das pessoas e criar ambientes mais agradáveis a todos!
                 {{--<p align="justify"><button class="btn-u btn-u-lg btn-u-red btn-u-upper" type="submit">Get In Touch</button>--}}
                 </p>
             </div>
@@ -19,9 +20,16 @@
                         <i class="icon-custom icon-2lg rounded-x icon-color-u icon-border-v2 icon-line icon-magnet"></i>
                     </div>
                     <div class="g-display-td g-text-top">
-                        <h4>Parceria</h4>
-                        <p align="justify">O projeto bem elaborado é resultado da parceria entre profissional e clientes.
-                        É preciso entender bem as necessidades, sonhos e idéias do cliente para em conjunto chegar à uma solução que alie técnica, funcionalidade e estética.
+                        <h4>FOCO NA REALIZAÇÃO DE SONHOS</h4>
+                        <p align="justify">Ao trabalhar com projetos estamos materializando os sonhos dos clientes. Por entender 
+                        nossa responsabilidade nesse processo levamos a sério cada etapa, buscando soluções completas, dentro dos 
+                        padrões esperados de qualidade e custo. Entendemos que o projeto bem elaborado é resultado da parceria entre 
+                        profissional e clientes, por isso buscamos entender as necessidades e idéias do cliente para em conjunto 
+                        chegar à uma solução que alie técnica, funcionalidade e estética. Cada projeto é único, cada solução é única 
+                        e pensada com carinho para atender cada um de nossos clientes. A escolha do material, o estilo adotado, 
+                        cada medida, tudo é pensado de modo que o usuário crie total identificação com o seu espaço, que se sinta 
+                        feliz e realizado! Nosso escritório cuida de tudo com todo carinho e comprometimento que os sonhos dos nossos 
+                        clientes merecem!
                     </div>
                 </div>
                 <div class="g-display-table wow fadeInRight" data-wow-offset="20" data-wow-duration="1.75s">
@@ -29,9 +37,14 @@
                         <i class="icon-custom icon-2lg rounded-x icon-color-u icon-border-v2 icon-line icon-magic-wand"></i>
                     </div>
                     <div class="g-display-td g-text-top">
-                        <h4>Apresentação 3D e VR</h4>
-                        <p align="justify">Para melhor entendimento das soluções apresentadas, todos os projetos são entregues com perspectivas 3D (imagens que simulam o ambiente pronto) e imagens 360° (com visualização total do espaço).
-                        Assim, é possível compreender cada detalhe de forma clara</p>
+                        <h4>k.	APRESENTAÇÃO 3D E VR</h4>
+                        <p align="justify">Entendemos que o desenvolvimento de um projeto - seja ele comercial, residencial, corporativo, etc... – 
+                        gera muita ansiedade e insegurança aos clientes, o que é perfeitamente aceitável quando pensamos que geralmente eles estão 
+                        depositando ali além do investimento financeiro, também sonhos e expectativas. Para que o cliente possa se sentir seguro 
+                        em aprovar as soluções propostas, trabalhamos com todo carinho a primeira etapa de apresentação, pois entendemos que essa é 
+                        a etapa mais importante para todo o processo. Com o desenvolvimento de imagens 3D e inovando através da realidade virtual, 
+                        é possível facilitar a compreensão de cada detalhe da proposta. Assim, o cliente poderá entender o funcionamento e sentir 
+                        o espaço projetado antes mesmo da sua execução! </p>
                     </div>
                 </div>
                 <div class="g-display-table wow fadeInRight" data-wow-offset="20" data-wow-duration="1.75s">
@@ -39,12 +52,16 @@
                         <i class="icon-custom icon-2lg rounded-x icon-color-u icon-border-v2 icon-line icon-calculator"></i>
                     </div>
                     <div class="g-display-td g-text-top">
-                        <h4>Planejamento</h4>
-                        <p align="justify">Para que o cliente fique satisfeito é necessário mais do que um projeto bem feito, é necessário que se tenha organização, planejamento de prazos e custos.
-                        Compromisso com todas as etapas até a entrega final é essencial para uma obra bem executada.</p>
+                        <h4>QUALIDADE DE PROJETO E GESTÃO</h4>
+                        <p align="justify">Entendemos que para o cliente ficar satisfeito e tranquilo quanto à execução de seu projeto 
+                        é necessário muito mais do que um projeto bonito, é preciso que ele esteja corretamente representado e 
+                        documentado para que se garanta a compatibilidade total entre o previsto e o executado. O trabalho desenvolvido 
+                        por nosso escritório é completo e engloba todas as fases de elaboração do espaço, incluindo fases de projeto e 
+                        também de execução. Com organização e critérios de qualidade, planejamos tudo para que se atenda às previsões 
+                        de custo e prazo! Pensamos em tudo para que você se sinta tranquilo para curtir seu novo espaço!</p>
                     </div>
                 </div>
-                <div class="g-display-table wow fadeInRight" data-wow-offset="20" data-wow-duration="1.75s">
+                <!--<div class="g-display-table wow fadeInRight" data-wow-offset="20" data-wow-duration="1.75s">
                     <div class="g-display-td g-text-middle hidden-xs">
                         <i class="icon-custom icon-2lg rounded-x icon-color-u icon-border-v2 icon-line icon-calculator"></i>
                     </div>
@@ -52,7 +69,7 @@
                         <h4>Acompanhamento online</h4>
                         <p class="custom-color-white">Com login e senha no site cada cliente terá acesso às informações do seu projeto como acompanhamento de etapas, cronograma, contato de fornecedores, download de material e acompanhamento financeiro. (em andamento - prev.: 2017)</p>
                     </div>
-                </div>
+                </div>-->
             </div>
         </div>
     </div>
