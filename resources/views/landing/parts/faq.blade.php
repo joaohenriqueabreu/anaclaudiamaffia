350f6139a593efb5477513bbd674c29cc1e22de5
anacmaffia <arq.anaclaudiamaffia@gmail.com>
2016-12-09 15:18:51 -0200

    texto

--- resources/views/landing/parts/faq.blade.php
+++ resources/views/landing/parts/faq.blade.php
@@ -5,27 +5,27 @@
                 <div class="twi">
                     <a href="#" class="twi-author g-mb-25">
                         {{--<img src="assets/img-temp/twi/1.jpg" alt="">--}}
-                        {{ Html::image(asset('img/faq/twi/1.jpg')) }}
+                        {{ Html::image(asset('img/faq/barra4.jpg')) }}
                         <strong>@unify</strong>
                     </a>
                     <blockquote class="twi__tweet g-mb-30">
-                        In eu augue massa. Phasellus <a href="#">#rutrum</a> velit diam, quis pellentesque libero hendrerit a.
+                        Quanto custa?
                     </blockquote>
                     <em class="twi__date">
-                        12:35 Pm, June 12, 2015
+                        A consultoria tem um valor único e reduzido se comparado aos demais serviços, pois é um trabalho desenvolvido com duração pré-definida. Já o projeto completo ou o projeto virtual dependerá das suas necessidades. Entre em contato conosco para um orçamento sem compromisso!
                     </em>
                 </div>
                 <div class="twi">
                     <a href="#" class="twi-author g-mb-25">
                         {{--<img src="assets/img-temp/twi/1.jpg" alt="">--}}
-                        {{ Html::image(asset('img/faq/twi/1.jpg')) }}
+                        {{ Html::image(asset('img/faq/barra4.jpg')) }}
                         <strong>@unify</strong>
                     </a>
                     <blockquote class="twi__tweet g-mb-30">
-                        In eu augue massa. Phasellus <a href="#">#rutrum</a> velit diam, quis pellentesque libero hendrerit a.
+                        Qual a diferença entre o projeto completo e a consultoria?
                     </blockquote>
                     <em class="twi__date">
-                        12:35 Pm, June 12, 2015
+                        A consultoria é um trabalho mais rápido e direcionado, em que conversamos sobre todas as necessidades do espaço, mas a execução fica por conta do cliente. Já o cliente que opta por contratar o projeto completo, receberá o pacote completo com imagens e detalhamento de execução, acompanhamento de orçamento e contratação de fornecedores, auxílio com dúvidas, acompanhamento da execução, etc. Veja aqui os detalhes de cada um dos serviços!
                     </em>
                 </div>
             </div>
