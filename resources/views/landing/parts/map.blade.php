56eefa169898af8228ce02bb3ab6917d05b4b691
anacmaffia <arq.anaclaudiamaffia@gmail.com>
2016-12-09 13:11:44 -0200

    exclusao barra endereço

--- resources/views/landing/parts/map.blade.php
+++ resources/views/landing/parts/map.blade.php
@@ -4,7 +4,7 @@
 </div>
 </div>
 <div class="GMap-stand-alone" id="map"></div>
-<div class="text-center margin-top-20 margin-bottom-20 custom-color-ana-green font-bold">
+<!--<div class="text-center margin-top-20 margin-bottom-20 custom-color-ana-green font-bold">
     Rua dos Timbiras, 2683 - Funcionários, Belo Horizonte, MG.<br>
     <i class="fa fa-clock-o"></i>  08:00 às 18:00
-</div>
+</div>--!>
